6ab59b800316bef9559fe7e6e23a930a1e9e9506
rainingdaewoo <78574530+rainingdaewoo@users.noreply.github.com>
2021-11-24 19:09:27 +0900

    Add files via upload
    2021.11.24

---
+++ baekjoon/backjoon_2588.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class backjoon_2588 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		 
+		int A = in.nextInt();
+		String B = in.next();
+        
+		in.close();
+ 
+		System.out.println(A * (B.charAt(2) - '0'));
+		System.out.println(A * (B.charAt(1) - '0'));
+		System.out.println(A * (B.charAt(0) - '0'));
+		System.out.println(A * Integer.parseInt(B));
+	}
+
+}
---
+++ baekjoon/baekjoon_1330.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon_1330 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		 
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		if(A > B) {
+			System.out.println(">");
+		} else if(A < B) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+		
+	}
+
+}
