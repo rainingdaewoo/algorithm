e7479a0e5e9b99c6e7913a0758cac31c29107bc9
김우중 <rainingdaewoo@gmail.com>
2022-05-11 21:17:41 +0900

    Add files via upload

---
+++ Backjoon_18108.java
@@ -0,0 +1,13 @@
+package com.board.controller.Backjoon_20220511;
+
+import java.util.Scanner;
+
+public class Backjoon_18108 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int input = s.nextInt() - 543;
+		System.out.println(input );	
+	}
+
+}
