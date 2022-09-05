3e82c9b4e54adbcaf5a64ed3460239dd0808cbe4
김우중 <rainingdaewoo@gmail.com>
2022-09-05 15:51:47 +0900

    2022.09.05 - 프로그래머스 문제 풀이

---
+++ 프로그래머스/문자열내p와y의개수.java
@@ -0,0 +1,32 @@
+package 프로그래머스;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Solution {
+
+    public boolean solution(String s) {
+        boolean answer = true;
+        int y = 0;
+        int p = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if( s.charAt(i) == 'y' || s.charAt(i) == 'Y' ) {
+                y++;
+            } else if( s.charAt(i) == 'p' || s.charAt(i) == 'P' ) {
+                p++;
+            }
+        }
+        if(y != p) {
+            answer = false;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "pPoooyY";
+        Solution sol = new Solution();
+        System.out.println(sol.solution(s));
+
+    }
+}
