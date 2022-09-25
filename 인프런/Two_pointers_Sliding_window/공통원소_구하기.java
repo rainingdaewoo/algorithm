2e28574e3f2a7a029485121ba39a8db2ff5d15a2
김우중 <rainingdaewoo@gmail.com>
2022-09-25 22:34:01 +0900

    2022.09.25 - 인프런 문제 풀이

--- 인프런/Two_pointers_Sliding_window/공통원소_구하기.java
+++ 인프런/Two_pointers_Sliding_window/공통원소_구하기.java
@@ -5,20 +5,21 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class 공통원소_구하기 {
-    public static int[] solution(int N, int M, int[] arr1, int[] arr2) {
-        ArrayList<Integer> sample = new ArrayList<>();
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                if(arr1[i] == arr2[i]){
-                    sample.add(arr1[i]);
-                }
-            }
-        }
-        int[] answer = new int[sample.size()];
+    public static ArrayList<Integer> solution(int N, int M, int[] arr1, int[] arr2) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
 
-        for(int i = 0; i < answer.length; i++) {
-            System.out.println(sample.get(i));
-            answer[i] = sample.get(i);
+        int point1 = 0, point2 = 0;
+        while(point1 < N && point2 < M) {
+            if(arr1[point1] == arr2[point2]) {
+                answer.add(arr1[point1++]);
+                point2++;
+            } else if(arr1[point1] < arr2[point2]){
+                point1++;
+            } else {
+                point2++;
+            }
         }
         return answer;
     }
@@ -27,10 +28,12 @@ public class 공통원소_구하기 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] arr1 = new int[N];
+
         for(int i = 0; i < N; i++) {
             arr1[i] = sc.nextInt();
         }
         int M = sc.nextInt();
+
         int[] arr2 = new int[M];
         for(int i = 0; i < M; i++) {
             arr2[i] = sc.nextInt();
